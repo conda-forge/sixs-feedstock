f5c259bb5170068d9eb31be279987449d2c3a7b9
Filipe Fernandes <ocefpaf@gmail.com>
2019-03-13 16:31:57 -0300

    add an extra test

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,8 +3,7 @@
 # Avoid weird messages that confuse py6s
 # export FFLAGS="-ffpe-summary=none"
 
-cmake -D CMAKE_INSTALL_PREFIX=$PREFIX \
-.
+cmake -D CMAKE_INSTALL_PREFIX=$PREFIX $SRC_DIR
 
 make
 make install
