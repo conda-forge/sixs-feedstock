32e50b738a4cd50c33b5f78c02e71a6a936d11c1
Isuru Fernando <isuruf@gmail.com>
2019-03-13 15:29:39 -0500

    Add debug info flag

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,8 +2,9 @@
 
 # Avoid weird messages that confuse py6s
 # export FFLAGS="-ffpe-summary=none"
+export FFLAGS="-g $FFLAGS"
 
 cmake -D CMAKE_INSTALL_PREFIX=$PREFIX $SRC_DIR
 
-make
+make -j${CPU_COUNT}
 make install
