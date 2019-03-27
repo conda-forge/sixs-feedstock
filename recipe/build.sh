4f22132a2f3ed949b80598b3e3b9f5190e7c6472
Filipe Fernandes <ocefpaf@gmail.com>
2019-03-27 08:18:52 -0300

    use -ffpe-summary=none only on Linux

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 export FFLAGS=$(echo "${FFLAGS}" | sed "s/-fopenmp//g")
-export FFLAGS="-ffpe-summary=none ${FFLAGS}"
+
+
+if [[ $(uname) == Linux ]]; then
+  export FFLAGS="-ffpe-summary=none ${FFLAGS}"
+fi
 
 cmake -D CMAKE_INSTALL_PREFIX=$PREFIX $SRC_DIR
 
